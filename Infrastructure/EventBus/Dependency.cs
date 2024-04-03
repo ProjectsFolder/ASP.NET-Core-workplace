8bcc02a4cb39b35628f65ab2c47b0a8e13ad8740
asylum29 <ksenon3@mail.ru>
2024-04-03 18:13:44 +0700

    rabbit retry connection

--- Infrastructure/EventBus/Dependency.cs
+++ Infrastructure/EventBus/Dependency.cs
@@ -11,19 +11,34 @@ public static class Dependency
     {
         services.AddSingleton(services =>
         {
-            var factory = new ConnectionFactory
+            var retries = 3;
+            while (true)
             {
-                HostName = configuration["Mq:Host"],
-                UserName = configuration["Mq:Login"],
-                Password = configuration["Mq:Password"],
-                DispatchConsumersAsync = true,
-            };
-            if (int.TryParse(configuration["Mq:Port"], out int tcpPort))
-            {
-                factory.Port = tcpPort;
-            }
+                try
+                {
+                    var factory = new ConnectionFactory
+                    {
+                        HostName = configuration["Mq:Host"],
+                        UserName = configuration["Mq:Login"],
+                        Password = configuration["Mq:Password"],
+                        DispatchConsumersAsync = true,
+                    };
+                    if (int.TryParse(configuration["Mq:Port"], out int tcpPort))
+                    {
+                        factory.Port = tcpPort;
+                    }
 
-            return factory.CreateConnection();
+                    return factory.CreateConnection();
+                }
+                catch
+                {
+                    if (--retries == 0)
+                    {
+                        throw;
+                    }
+                    Thread.Sleep(5000);
+                }
+            }
         });
         services.AddHostedService<RabbitMqListener>();
         services.AddScoped<IRabbitMq, RabbitMq>();
